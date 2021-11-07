4abf2db826a13c8301f3f6d830c05bc1b047642e
Toby Crawley <toby@tcrawley.org>
2021-11-07 12:48:45 -0500

    Initial doom emacs config

---
+++ functions.el
@@ -0,0 +1,8 @@
+;;; functions.el -*- lexical-binding: t; -*-
+
+(defun tc/turn-on-paredit ()
+  (paredit-mode t)
+  (diminish 'paredit-mode)
+  (when (not (display-graphic-p))
+    (define-key paredit-mode-map (kbd "M-[ c") 'paredit-forward-slurp-sexp)
+    (define-key paredit-mode-map (kbd "M-[ d") 'paredit-forward-barf-sexp)))
