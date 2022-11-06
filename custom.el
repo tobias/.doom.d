8e58316f00decdb7aaa5ace0057fa93f86c935d7
Toby Crawley <toby@tcrawley.org>
2022-11-06 09:35:55 -0500

    Store customizations

---
+++ custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((org-element-cache) defvaralias)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t (:background "#000000" :foreground "#ffff00" :weight ultra-bold)))))
